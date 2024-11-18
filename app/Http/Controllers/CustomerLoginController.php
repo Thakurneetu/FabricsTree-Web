7163a30643c024a23e48a2eb3d6e6733fd8048e4
Deepak Patidar <deepak07patidar@gmail.com>
2024-11-18 17:06:37 +0530

    login form update

--- app/Http/Controllers/CustomerLoginController.php
+++ app/Http/Controllers/CustomerLoginController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
+
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -10,6 +11,8 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Str;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerLoginController extends Controller
 {
@@ -30,62 +33,187 @@ class CustomerLoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/dashboard';
-
+    protected $redirectTo = '/home';
+   
+    
     /**
-     * Create a new controller instance.
+     * Handle a login request to the application.
      *
-     * @return void
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
      */
-    public function __construct()
+    public function login(Request $request)
     {
-        parent::__construct();
-       // $this->middleware('guest')->except('logout');
+        $this->validateLogin($request);
 
-    }
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
 
-    protected function credentials(\Illuminate\Http\Request $request)
-    {
-        //return $request->only($this->username(), 'password');
-        return [
-            'email' => $request->{$this->username()},
-            'password' => $request->password,
-           // 'status' => 'active'
-        ];
-    }
-
-    protected function validateLogin(\Illuminate\Http\Request $request)
-    {
-
-        $rules = [
-            $this->username() => 'required|string',
-            'password' => 'required|string'
-        ];
-        // User type from email/username
-        $user = Customer::where($this->username(), $request->{$this->username()})->first();
-        if(!empty($user)){
-            if(@$user->status=='inactive'){
-                throw ValidationException::withMessages([
-                    $this->username() => [trans('auth.inactive')],
-                ]);
-            }
+            return $this->sendLockoutResponse($request);
         }
 
-        $this->validate($request, $rules);
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
-    
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            $this->username() => 'required|string',
+            'pwd' => 'required|string',
+        ]);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request), $request->boolean('remember')
+        );
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        return $request->only($this->username(), 'pwd');
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+                    ? new JsonResponse([], 204)
+                    : redirect()->intended($this->redirectPath());
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        //
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        throw ValidationException::withMessages([
+            $this->username() => [trans('auth.failed')],
+        ]);
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
+    }
+
+    /**
+     * Log the user out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
     public function logout(Request $request)
     {
-        $user = auth()->user();
         $this->guard()->logout();
 
         $request->session()->invalidate();
 
-        if ($user->is_superadmin) {
-            return $this->loggedOut($request) ?: redirect(route('front.super-admin-login'));
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
         }
-        return redirect(route('login'));
-        
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect('/');
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard('customer');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::post('customer/register', [App\Http\Controllers\CustomerController::class, 'register'])->name('customer.register');
-//Route::post('customer/login', [App\Http\Controllers\CustomerLoginController::class, 'login'])->name('customer.login');
-//Route::post('customer/forgotpassword', [App\Http\Controllers\CustomerController::class, 'forgotpassword'])->name('customer.forgotpassword');
-//Route::post('customer/generatepassword', [App\Http\Controllers\CustomerController::class, 'generatepassword'])->name('customer.generatepassword');
+Route::post('customer/login', [App\Http\Controllers\CustomerLoginController::class, 'login'])->name('customer.login');
+Route::post('customer/forgotpassword', [App\Http\Controllers\CustomerController::class, 'forgotpassword'])->name('customer.forgotpassword');
+Route::post('customer/generatepassword', [App\Http\Controllers\CustomerController::class, 'generatepassword'])->name('customer.generatepassword');
 
 Route::group(['middleware' => 'auth'], function () {
   Route::prefix('admin')->name('admin.')->group(function() {
