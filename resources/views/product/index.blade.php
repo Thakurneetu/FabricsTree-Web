72d9d757b62ac791e729bc3fab2a4360691eb433
Deepak Patidar <deepak07patidar@gmail.com>
2025-03-04 11:10:51 +0530

    address update

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -253,7 +253,8 @@
 
                     <div class="row">
                         <div class="col-md-12 col-sm-12">
-                          <form id="customForm">
+                          <form id="customForm" method="POST">
+                          @csrf
                             <div class="mb-3 registerss">
                             
                                 <label for="FabricCategory" style="font-weight: bold;">Category</label>
