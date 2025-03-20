230c25130c65a3100f56bcaf2a15832232894f39
Dheerajzxx <dheerajzxx@gmail.com>
2025-03-20 11:06:04 +0530

    bug fix

--- app/DataTables/ContactUsDataTable.php
+++ app/DataTables/ContactUsDataTable.php
@@ -2,7 +2,7 @@
 
 namespace App\DataTables;
 
-use App\Models\ContactUS;
+use App\Models\ContactUs;
 use Illuminate\Database\Eloquent\Builder as QueryBuilder;
 use Yajra\DataTables\EloquentDataTable;
 use Yajra\DataTables\Html\Builder as HtmlBuilder;
